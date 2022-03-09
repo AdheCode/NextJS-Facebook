35481897eac095fe63f724c3c78e96ba50ba6b7a
AdheCode <adhep86@gmail.com>
2022-03-09 12:17:37 +0700

    fix: :bug: env secret facebook

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   serverRuntimeConfig: {
     // Will only be available on the server side
-    facebook_id: process.env.FACEBOOK_ID,
-    facebook_secret: process.env.FACEBOOK_SECRET,
+    facebook_id: process.env.NEXT_PUBLIC_FACEBOOK_ID,
+    facebook_secret: process.env.NEXT_PUBLIC_FACEBOOK_SECRET,
     api_key: process.env.API_KEY,
   },
   images: {
